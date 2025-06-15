f637cf63ca7e9a913d6348a61de69bc95f63c23d
cohoichota <cohoichota@gmail.com>
2025-06-15 16:35:29 +0700

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'core/theme/theme.dart';
+import 'features/auth/presentation/pages/signup_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Blog App',
+      theme: AppTheme.darkThemeMode,
+      home: const SignUpPage(),
+    );
+  }
+}
